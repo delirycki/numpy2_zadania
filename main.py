767c552647463e9bb01917dc85a7db20f0462d93
Pawel Komarnicki <pawelkomarnicki@MacBook-Air-Pawel.local>
2021-04-21 22:24:17 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+import numpy as np
+# #ZAD1
+# a = np.array([3,4,3])
+# b = np.array([2,5,6])
+#
+#
+# print("A*B {} B*A {}".format(str(a*b),str(b*a)))
+
+# # #ZAD2
+# a=np.arange(9).reshape(3,3)
+# b=np.arange(16).reshape(4,4)
+#
+# print(a.min(axis=1))
+# print(b.min(axis=1))
+
+# #ZAD3
+# a = np.array([3,4,3])
+# b = np.array([2,5,6])
+#
+#
+# print("A*B {} B*A {}".format(str(a.dot(b)),str(b.dot(a))))
+
+#zad4
+# a = np.array([2,3,4])
+# b = np.array([2.3,4.5,3.3])
+#
+# print("A*B {} B*A {}".format(str(a*b),str(b*a)))
+#zad5
+a = np.arange(6).reshape(3,2)
+a = np.sin(a)
+#zad6
+b = np.arange(6).reshape(3,2)
+b = np.cos(a)
+
+
