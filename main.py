6353051443b8cbb5d90fb741eabca1b235011941
delirycki <81227443+delirycki@users.noreply.github.com>
2021-04-22 09:28:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -25,11 +25,33 @@ import numpy as np
 # b = np.array([2.3,4.5,3.3])
 #
 # print("A*B {} B*A {}".format(str(a*b),str(b*a)))
-#zad5
-a = np.arange(6).reshape(3,2)
-a = np.sin(a)
-#zad6
-b = np.arange(6).reshape(3,2)
-b = np.cos(a)
+# #zad5
+# a = np.arange(6).reshape(3,2)
+# a = np.sin(a)
+# #zad6
+# b = np.arange(6).reshape(3,2)
+# b = np.cos(a)
+#
+# #zad7
+# c=a+b
+# print(c)
+
+#zad8
+# a = np.arange(9).reshape(3,3)
+# for i in a:
+#     print(i)
+#zad9
+# a = np.arange(9).reshape(3,3)
+# for i in a.flat:
+#     print(i)
+#zad11
+# a= np.arange(12)
+# b= a.reshape(3,4)
+# c= a.reshape(4,3)
+# d= a.reshape(2,6)
+# print(a)
+# print(b)
+# print(c)
+# print(d)
 
 
